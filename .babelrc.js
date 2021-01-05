251a327d94230798c63986efd92f5ebff3179bf9
sunss <sss@dataqin.com>
2021-01-05 17:27:01 +0800

    📦 新建webpack项目

---
+++ .babelrc.js
@@ -0,0 +1,22 @@
+// bable 编译器
+
+module.exports = {
+  presets: [
+    // jsx 编译器 https://www.babeljs.cn/docs/babel-preset-react
+    [
+      "@babel/preset-react",
+      {
+        development: process.env.BABEL_ENV === "development",
+      },
+    ],
+    // ES6 ES7 编译器 https://www.babeljs.cn/docs/babel-preset-env
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          esmodules: true,
+        },
+      },
+    ],
+  ],
+};
